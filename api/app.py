b83fc3cc43e8ee91c4ba55e4b2fdce8b3498e4fe
speedcode860 <speedcode860@gmail.com>
2026-06-30 04:25:02 +0000

    Add Redis service and connect API to Redis

--- api/app.py
+++ api/app.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify
+import redis
+import os
 
 app = Flask(__name__)
+
+# Connexion Redis
+redis_host = os.getenv("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
 @app.route("/health")
 def health():
     return jsonify({"status": "ok"})
 
-@app.route("/")
-def home():
-    return jsonify({"message": "Reaction Game API running"})
+@app.route("/cache-test")
+def cache_test():
+    count = r.incr("hits")
+    return jsonify({"redis_hits": count})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
